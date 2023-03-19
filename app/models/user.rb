92dd3dfef02500f1adf548efd449ed1140d148e6
teguchi0920 <childrenkey@gmail.com>
2023-03-19 23:25:26 +0900

    Userモデルにバリデーションを記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates_presence_of :nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birthday
 end
