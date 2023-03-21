fed86d8160063f24842005b35532968538542b67
teguchi0920 <childrenkey@gmail.com>
2023-03-21 22:58:31 +0900

    パスワード、名前、名前（カナ）にバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates_presence_of :nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birthday
+  validates_presence_of :nickname, :birthday
+  validates :password, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+  validates :first_name, :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+  validates :first_name_kana, :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/ }
+  
 end
