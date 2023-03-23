4786368c00331701285b3573108d9a311f6b0fe3
teguchi0920 <childrenkey@gmail.com>
2023-03-23 23:23:23 +0900

    Fakerの導入

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,17 @@
 FactoryBot.define do
   factory :user do
-    nickname {'フリマ太郎'}
-    email {'test@example'}
-    password {'t11111'}
-    encrypted_password {'t11111'}
-    first_name {'フリマ'}
-    last_name {'太郎'}
-    first_name_kana {'フリマ'}
-    last_name_kana {'タロウ'}
-    birthday {'1999-10-10'}
+    transient do
+      person {Gimei.name}
+    end
+
+    nickname {Faker::Name.initials(number: 4)}
+    email {Faker::Internet.free_email}
+    password {'ps' + Faker::Internet.password(min_length: 6)}
+    encrypted_password {password}
+    last_name {person.last.kanji}
+    first_name {person.first.kanji}
+    last_name_kana {person.last.katakana}
+    first_name_kana {person.first.katakana}
+    birthday {Faker::Date.between(from: '1930-01-01', to: '2018-12-31')}
   end
 end
