2a4d9f0ea89e268cf7c40ba625acab79a0a6106b
teguchi0920 <childrenkey@gmail.com>
2023-03-28 21:58:01 +0900

    配送料の負担のプルダウンの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,11 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :item_image
+  
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :item_category
   belongs_to :item_condition
+  belongs_to :shipping_fee
 
   validates_presence_of :item_name, :item_info, :shipping_fee_id, :prefecture_id, :days_to_ship_id, :item_price
   validates :item_category_id, :item_condition_id, numericality: {other_than: 1, message: "can't be blank"}
