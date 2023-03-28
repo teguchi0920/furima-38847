04a1613ece2c8d8ec01a8b396d6ac49104187e5a
teguchi0920 <childrenkey@gmail.com>
2023-03-28 21:46:36 +0900

    商品の状態のプルダウンの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,8 +3,9 @@ class Item < ApplicationRecord
   has_one_attached :item_image
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :item_category
+  belongs_to :item_condition
 
-  validates_presence_of :item_name, :item_info, :item_condition_id, :shipping_fee_id, :prefecture_id, :days_to_ship_id, :item_price
-  validates :item_category_id, numericality: {other_than: 1, message: "can't be blank"}
+  validates_presence_of :item_name, :item_info, :shipping_fee_id, :prefecture_id, :days_to_ship_id, :item_price
+  validates :item_category_id, :item_condition_id, numericality: {other_than: 1, message: "can't be blank"}
 
 end
