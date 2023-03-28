bbd8d8e16a2a659f59a52da615a6798a5f2e7a17
teguchi0920 <childrenkey@gmail.com>
2023-03-28 22:44:12 +0900

    発送までの日数のプルダウンの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,9 +7,10 @@ class Item < ApplicationRecord
   belongs_to :item_condition
   belongs_to :shipping_fee
   belongs_to :prefecture
+  belongs_to :days_to_ship
 
-  validates_presence_of :item_name, :item_info, :days_to_ship_id, :item_price
-  validates :item_category_id, :item_condition_id, :shipping_fee_id, :prefecture_id,
+  validates_presence_of :item_name, :item_info, :item_price
+  validates :item_category_id, :item_condition_id, :shipping_fee_id, :prefecture_id, :days_to_ship_id,
    numericality: {other_than: 1, message: "can't be blank"}, 
    presence: true
 
