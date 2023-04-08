7a70802b278db40e2d5b0153922b6b8cd5943816
teguchi0920 <childrenkey@gmail.com>
2023-04-08 14:30:04 +0900

    同一のコードをbefore_actrionでまとめるよう記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -19,18 +20,15 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
     unless current_user.id == @item.user_id
       redirect_to action: :index
     end
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
@@ -45,4 +43,8 @@ class ItemsController < ApplicationController
       :image, :item_name, :item_info, :item_category_id, :item_condition_id, :shipping_fee_id, :prefecture_id, :days_to_ship_id, :item_price
     ).merge(user_id: current_user.id)
   end 
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
 end
