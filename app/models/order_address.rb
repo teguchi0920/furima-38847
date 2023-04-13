a59fa78d0b8acf2ae24971a3275525ba79387023
teguchi0920 <childrenkey@gmail.com>
2023-04-13 23:03:27 +0900

    Formオブジェクトのバリデーションを修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -2,7 +2,7 @@ class OrderAddress
   include ActiveModel::Model
   attr_accessor :user_id, :item_id, :post_code, :prefecture_id, :city, :block, :building, :phone_number, :order_id
 
-  with_options presece: true do
+  with_options presence: true do
     validates :user_id
     validates :item_id
     validates :post_code, format: {with: /\A\d{3}[-]\d{4}\z/, message: "is invalid. Include hyphen(-)"}
@@ -12,7 +12,6 @@ class OrderAddress
     validates :phone_number, format: {with: /\A\d{10,11}\z/, message: "is invalid. Input only number"}
     validates :order_id
   end
-  validates :building
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
