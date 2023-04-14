8258401846a8c7ceb15986a906f620ee492f3285
teguchi0920 <childrenkey@gmail.com>
2023-04-14 22:02:24 +0900

    form_withから送信される配送先のカラム名を変更

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -6,7 +6,7 @@ class OrderAddress
     validates :user_id
     validates :item_id
     validates :post_code, format: {with: /\A\d{3}[-]\d{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
+    validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
     validates :city
     validates :block
     validates :phone_number, format: {with: /\A\d{10,11}\z/, message: "is invalid. Input only number"}
