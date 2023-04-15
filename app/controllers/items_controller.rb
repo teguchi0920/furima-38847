8083cd7baaaf7e5ef3bb1f62295d363961f18c43
teguchi0920 <childrenkey@gmail.com>
2023-04-15 21:49:55 +0900

    N+1問題の解消

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,7 +4,7 @@ class ItemsController < ApplicationController
   before_action :move_to_index, only: [:edit, :destroy]
 
   def index
-    @items = Item.all.order('created_at DESC')
+    @items = Item.includes(:user).order('created_at DESC')
   end
 
   def new
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,12 +1,12 @@
 class OrdersController < ApplicationController
   def index
     @order_address = OrderAddress.new
-    @item = Item.find(params[:item_id])
+    @item = Item.includes(:user).find(params[:item_id])
   end
   
   def create
     @order_address = OrderAddress.new(order_params)
-    @item = Item.find(params[:item_id])
+    @item = Item.includes(:user).find(params[:item_id])
     if @order_address.valid?
       @order_address.save
       redirect_to root_path
