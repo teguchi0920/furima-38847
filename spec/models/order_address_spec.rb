71338c8090703788cc66b33f85e80038abdbf25d
teguchi0920 <childrenkey@gmail.com>
2023-04-15 22:11:40 +0900

    テストコードの記述

--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -15,28 +15,64 @@ RSpec.describe OrderAddress, type: :model do
     end
     context '商品が購入できない場合' do
       it 'post_codeが空では登録できない' do
+        @order.post_code = ''
+        @order.valid?
+        expect(@order.errors.full_messages).to include("Post code can't be blank")
       end
       it 'post_codeに半角数字以外が含まれている場合は登録できない' do
+        @order.post_code = '１２３ー４５６７'
+        @order.valid?
+        expect(@order.errors.full_messages).to include("Post code is invalid. Include hyphen(-)")
       end
       it 'post_codeは3桁ハイフン4桁の半角数字でなければ登録できない' do
+        @order.post_code = '1234-567'
+        @order.valid?
+        expect(@order.errors.full_messages).to include("Post code is invalid. Include hyphen(-)")
       end
-      it 'item_category_idが1だと登録できない' do
+      it 'prefecture_idが1だと登録できない' do
+        @order.prefecture_id = 1
+        @order.valid?
+        expect(@order.errors.full_messages).to include("Prefecture can't be blank")
       end
       it 'cityが空だと登録できない' do
+        @order.city = ''
+        @order.valid?
+        expect(@order.errors.full_messages).to include("City can't be blank")
       end
       it 'blockが空だと登録できない' do
+        @order.block = ''
+        @order.valid?
+        expect(@order.errors.full_messages).to include("Block can't be blank")
       end
       it 'phone_numberが空だと登録できない' do
+        @order.phone_number = ''
+        @order.valid?
+        expect(@order.errors.full_messages).to include("Phone number can't be blank")
       end
       it 'phone_numberが10桁未満では登録できない' do
+        @order.phone_number = '123456789'
+        @order.valid?
+        expect(@order.errors.full_messages).to include("Phone number is invalid. Input only number")
       end
       it 'phone_numberが12桁以上では登録できない' do
+        @order.phone_number = '123456789101'
+        @order.valid?
+        expect(@order.errors.full_messages).to include("Phone number is invalid. Input only number")
       end
       it 'phone_numberに半角数字以外が含まれている場合は登録できない' do
+        @order.phone_number = '１２３４５６７８９０'
+        @order.valid?
+        expect(@order.errors.full_messages).to include("Phone number is invalid. Input only number")
       end
       it 'userが紐付いていなければ登録できない' do
+        @order.user_id = nil
+        @order.valid?
+        expect(@order.errors.full_messages).to include("User can't be blank")
       end
       it 'itemが紐付いていなければ登録できない' do
+        @order.item_id = nil
+        @order.valid?
+        expect(@order.errors.full_messages).to include("Item can't be blank")
       end
     end
   end
