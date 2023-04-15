98a045cd70ce5c6315dfe40f0fa84133038791e3
teguchi0920 <childrenkey@gmail.com>
2023-04-15 19:36:28 +0900

    購入情報のFactoryBotを導入

---
+++ spec/factories/order_addresses.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+  Faker::Config.locale = :ja
+
+  factory :order_address do
+    post_code { Faker::Address.postcode }
+    prefecture_id { Faker::Number.between(from: 2, to: 48) }
+    city { Faker::Address.city }
+    block { Faker::Address.street_address }
+    building { Faker::Address.building_number}
+    phone_number {Faker::Number.number(digits: rand(10..11))}
+  end
+end
