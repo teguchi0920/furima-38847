89f47fecbdd3ab701526f197309a30a661026f11
teguchi0920 <childrenkey@gmail.com>
2023-04-16 18:49:49 +0900

    クレジットカード情報のトークン化

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,10 +1,26 @@
 const pay = () => {
+  const payjp = Payjp('pk_test_9a647ea059fea6ed986e955b')
+  const elements = payjp.elements();
+  const numberElement = elements.create('cardNumber');
+  const expiryElement = elements.create('cardExpiry');
+  const cvcElement = elements.create('cardCvc');
+
+  numberElement.mount('#card-number');
+  expiryElement.mount('#card-expiry');
+  cvcElement.mount('#card-cvc');
+
   const submit = document.getElementById("button");
   if (!submit){ return false;}
   submit.addEventListener("click", (e) => {
     e.preventDefault();
-    console.log("フォーム送信時にイベント発火")
+    payjp.createToken(numberElement).then(function (response){
+      if (response.error){
+      } else{
+        const token = response.id;
+        console.log(token)
+      }
+    });
   });
 };
-
+  
 window.addEventListener("load", pay);
