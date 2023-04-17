6cadc3744aafdc28d8636877cef65787759b0432
teguchi0920 <childrenkey@gmail.com>
2023-04-17 22:13:23 +0900

    Fakerの記述を修正

--- spec/factories/order_addresses.rb
+++ spec/factories/order_addresses.rb
@@ -5,9 +5,9 @@ FactoryBot.define do
     post_code { Faker::Address.postcode }
     prefecture_id { Faker::Number.between(from: 2, to: 48) }
     city { Faker::Address.city }
-    block { Faker::Address.street_address }
-    building { Faker::Address.building_number}
-    phone_number {Faker::Number.number(digits: rand(10..11))}
+    block { Faker::Address.street_name + Faker::Address.building_number }
+    building { Faker::Address.street_name + 'ハイム' + (Faker::Number.number(digits: 3)).to_s}
+    phone_number {Faker::Number.leading_zero_number(digits: rand(10..11))}
     token {'tok_' + Faker::Alphanumeric.alphanumeric(number: 28)}
   end
 end
